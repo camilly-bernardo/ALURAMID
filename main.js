23bf2efbc3209b1d1597f5ad8eff9b8357e02798
camilly-bernardo <127161037+camilly-bernardo@users.noreply.github.com>
2023-10-23 21:55:10 -0300

    alterando

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ function somTocaGrilo (){
 document.querySelector("#som_tecla_grilo").play();
 }
 document.querySelector(".tecla_grilo").onclick =somTocaGrilo;
+const listadeteclas=document.querySelectorAll(".tecla");
+listadeteclas[4].onclik=tocasomgrilo
